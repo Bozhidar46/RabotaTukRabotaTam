aa7952193032e4e0cb51c56aebe818bfe7cc6980
Bozhidar46 <bozhidarzlatanov05@pmgkk.com>
2024-04-16 23:29:22 +0300

    Categories 2

---
+++ RabotaTukRabotaTam/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabotaTukRabotaTam.Data;
+using RabotaTukRabotaTam.Models;
+
+namespace RabotaTukRabotaTam.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var entities = await context.Categories.ToListAsync();
+
+            var model = entities
+                .Select(c => new CategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    ImgURL = c.ImgURL
+
+                });
+
+            return View(model);
+        }
+    }
+}
---
+++ RabotaTukRabotaTam/Models/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RabotaTukRabotaTam.Models
+{
+    public class CategoryViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ImgURL { get; set; } = null!;
+    }
+}
