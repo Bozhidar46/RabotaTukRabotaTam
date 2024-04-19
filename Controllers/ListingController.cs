750032d754fe894acca04cd6522f4de786b8418a
Bozhidar46 <139571817+Bozhidar46@users.noreply.github.com>
2024-04-19 23:54:12 +0300

    Add files via upload

---
+++ Controllers/ListingController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabotaTukRabotaTam.Data.Models;
+using RabotaTukRabotaTam.Data;
+using RabotaTukRabotaTam.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RabotaTukRabotaTam.Controllers
+{
+
+    public class ListingController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ListingController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var listings = await context.Listings.ToListAsync();
+            return View(listings);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddListingViewModel model)
+        {
+            var listing = new Listing()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Description = model.Description,
+                Location = model.Location,
+                Salary = model.Salary,
+                FirmName=model.FirmName,
+                CategoryId = model.CategoryId
+
+            };
+            await context.Listings.AddAsync(listing);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid Id)
+        {
+            var listing = await context.Listings.FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (listing != null)
+            {
+                var viewModel = new UpdateListingViewModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = listing.Name,
+                    Description = listing.Description,
+                    Location = listing.Location,
+                    Salary = listing.Salary,
+                };
+                return await Task.Run(() => View("View", viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateListingViewModel model)
+        {
+            var listing = await context.Listings.FindAsync(model.Id);
+            if (listing != null)
+            {
+                listing.Name = model.Name;
+                listing.Description = model.Description;
+                listing.Location = model.Location;
+                listing.Salary = model.Salary;
+                listing.Category = model.Category;
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateListingViewModel viewModel)
+        {
+            var listing = await context.Listings.FindAsync(viewModel.Id);
+
+            if (listing != null)
+            {
+                context.Listings.Remove(listing);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllListingsByCategory(Guid categoryId)
+        {
+            var listings = await context.Listings.Where(l => l.CategoryId == categoryId).ToListAsync();
+            return View(listings);
+        }
+    }
+}
+
---
+++ Controllers/UserController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RabotaTukRabotaTam.Data.Models;
+using RabotaTukRabotaTam.Models;
+
+namespace Rabota_tuk__rabota_tam.Controllers
+{
+    [Authorize]
+    
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> userManager;
+
+        private readonly SignInManager<User> signInManager;
+
+        public UserController(UserManager<User> _userManager,SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = new User()
+            {
+                Email = model.Email,
+                UserName = model.UserName
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            var user = await userManager.FindByEmailAsync(viewModel.Email);
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, viewModel.Password, false, false);
+                if (result.Succeeded)
+                {
+                   
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid login");
+            return View(viewModel);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        /*
+           [Authorize]
+           public async ActionResult Roles()
+           {
+               var currentUser = await userManager.GetUserAsync(this.User);
+               var roles = await userManager.GetRolesAsync(currentUser);
+           }
+
+           [Authorize]
+           public async ActionResult Data()
+           {
+               var currentUser = await userManager.GetUserAsync(this.User);
+               var currentUserUsername = await userManager.GetUserNameAsync(currentUser);
+               var currentUserId = await userManager.GetUserIdAsync(currentUser);
+           }
+
+           public async Task<IActionResult> AddUserToRole()
+           {
+               var roleName = "Administrator";
+               var roleExists = await roleManager.RoleExistsAsync(roleName);
+
+               if (roleExists)
+               {
+                   var user = await userManager.GetUserAync(User);
+                   var result = await userManager.AddToRoleAsync(user, roleName);
+
+                   if (result.Succeeded)
+                   {
+
+                   }
+               }
+
+
+           }
+          */
+    }
+}
+
